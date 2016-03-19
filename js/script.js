c9403e3715bbca5f8534778b5ee1313ffddf1806
mendurim <mendurim.r@gmail.com>
2016-03-19 22:36:04 +0100

    Added the logic and background color changer

--- js/script.js
+++ js/script.js
@@ -1,71 +1,73 @@
-$(document).ready(function(){
-	$('.btn').click(function(){
-	$('#quoteText').append("<DL>\n" + "<DT>" + "\"" + quotes[index] + "\"</DT>\n" + "<DD>\t\t\t" + "-- " + authors[index] + "</DT>\n" +  "</DL>\n"); 
-	$('#quoteText').trigger('reset'); 	
-	}); 
-}); 
-
-
-
-
-
-var all = [
-	{quote: "I have a new philosophy. I'm only going to dread one day at a time.", author: "Charles Schulz"}, 
-	{quote: "Reality is the leading cause of stress for those in touch with it.", author: "Jack Wagner"}, 
-	{quote: "Few things are harder to put up with than the annoyance of a good example.", author: "Mark Twain"}, 
-	{quote: "The pure and simple truth is rarely pure and never simple.", author: "Oscar Wilde"}, 
-	{quote: "There's no business like show business, but there are several businesses like accounting.", author: "David Letterman"}, 
-	{quote: "Man invented language to satisfy his deep need to complain.", author: "Lily Tomlin"}, 
-	{quote: "Bad programmers worry about the code. Good programmers\n worry about data structures and their relationships.", author: "Linus Torvald"}, 
-	{quote: "Just-a-second, what commands do we have to put the left foot in the ground...?", author: "Mendurim Rashiti"} 
-]
-	
-var quotes = all.map(function(n){
-	return n.quote; 
-}); 
-var authors = all.map(function(a){
-	return a.author; 
-});
-	
-var index = Math.floor(Math.random()* quotes.length);     
+	$(document).ready(function(){
 	
 
+	var quoteDB=[
+		{quote: "I have a new philosophy. I'm only going to dread one day at a time.", author: "Charles Schulz"}, 
+		{quote: "Reality is the leading cause of stress for those in touch with it.", author: "Jack Wagner"}, 
+		{quote: "Few things are harder to put up with than the annoyance of a good example.", author: "Mark Twain"}, 
+		{quote: "The pure and simple truth is rarely pure and never simple.", author: "Oscar Wilde"}, 
+		{quote: "There's no business like show business, but there are several businesses like accounting.", author: "David Letterman"}, 
+		{quote: "Man invented language to satisfy his deep need to complain.", author: "Lily Tomlin"}, 
+		{quote: "Bad programmers worry about the code. Good programmers\n worry about data structures and their relationships.", author: "Linus Torvald"}, 
+		{quote: "Just-a-second, what commands do we have to put the left foot in the ground...?", author: "Mendurim Rashiti"}, 
+		{quote: "Walking on water and developing software from a specification are easy if both are frozen", author: "Edward V Berard"}, 
+		{quote: "Don’t worry if it doesn’t work right. If everything did, you’d be out of a job.", author: "Mosher’s Law of Software Engineering"}, 
+		{quote: "Talk is cheap. Show me the code.", author: "Linus Torvalds"}, 
+		{quote: "when you don't create things, you become defined by your tastes rather than ability. your tastes only narrow & exclude people. so create.", author: "Why The Lucky Stiff"},
+		{quote: "Software testing is a sport like hunting, it's bughunting.", author: "Amit Kalantri"},
 
+		]; 
+		
 
+		$('#quoteButton').click(function(evt){
+			//define the containers of the info we target
+			var quote = $('#quoteContainer p').text();
+			var quoteAuthor = $('#quoteAuthor').text();
+			changeBackground();
 
-
-
-
-
-
-
-
-
-	// document.write("<DL>\n");
-	// document.write("<DT>" + "\"" + quotes[index] + "\"</DT>\n");
-	// document.write("<DD>\t\t\t" + "-- " + authors[index] + "</DT>\n");
-	// document.write("</DL>\n");
+			//prevent browser's default action
+			evt.preventDefault();
+			//getting a new random number to attach to a quote and setting a limit
+			var quoteSource = quoteDB.length;
+			var randomNumber= Math.floor(Math.random()*quoteSource);
+			//set a new quote
+			for(i=0;i<=quoteSource;i+=1){
+			var newQuote = quoteDB[randomNumber].quote;
+			var newQuoteAuthor = quoteDB[randomNumber].author;
+			//console.log(newQuote,newQuoteAuthor);
+      		var timeAnimation = 100;
+      		var quoteContainer = $('#quoteContainer');
+	      		//fade out animation with callback
+	      		quoteContainer.fadeOut(timeAnimation, function(){
+	        		quoteContainer.html('');
+						quoteContainer.append('<p>'+newQuote+'</p>'+'<p id="quoteAuthor">'+'-'+newQuoteAuthor+'</p>');
+	        
+			        //fadein animation.
+			        quoteContainer.fadeIn(timeAnimation);
+			    });  
+			
+				break;
+			};//end for loop
 	
-	// console.log(quotes[0] + authors[0]);
-	// // var results = document.getElementById("quoteText"); 
-	// // results.innerHtml = '(<h3>' + quotes[0] + '</h3>)';
-	// // document.write('<div>'+ quotes[0] + authors[0] + '</div>'); 	
-	// document.getElementById('quoteText').innerHtml = ('<h3>' + quotes[0] + '</h3>');  
+		// generate numbers for changing background colors
+  		function generateRandomNum(min,max){
+    		return Math.floor(Math.random()*(max-min+1)+min);
+  		}
 
- // var quotes = []; 
- // var authors = []; 
- // for(var i=0; i < all.length; i++){
- // 	quotes.push(all[i]); 
- // debugger 
- // }
+	  	function changeBackground(){
+		    var colorR = generateRandomNum(0,250);
+		    var colorG = generateRandomNum(200,250);
+		    var colorB = generateRandomNum(200,250);
+		    var colorA = generateRandomNum(2,4);
 
 
-// document.getElementById('quoteText').innerHtml = quotes[0]; 
+		    $("body ").css("background-color","rgba("+ colorR + "," + colorG + "," + colorB + "," + colorA + ")").fadeIn(timeAnimation);
+		    $("#quoteButton").css("background-color","rgba("+ colorR + "," + colorG + "," + colorB + "," + colorA + ")").fadeIn(timeAnimation);
 
+	 	}
 
-// document.write("<DL>\n");
-// document.write("<DT>" + "\"" + quotes[0] + "\"</DT>\n");
-// document.write("<DD>" + "-- " + authors[i] + "</DT>\n");
-// document.write("</DL>\n");
-
+	});//end quoteButton function
+		
+		
+});//end document ready
 
